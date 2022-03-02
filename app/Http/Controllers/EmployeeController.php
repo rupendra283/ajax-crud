2119475a79947210cca1e6adeb0131805334553d
rupendra <rupendrasingh406@gmail.com>
2022-03-02 14:39:48 +0530

    working on add employee show employee

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // print_r($_POST);
+        // print_r($_FILES);
+
+        $file =  $request->file('avatar');
+        $fileName = time() . '.' . $file->getClientOriginalName();
+        $file->move(public_path('image/'),$fileName);
+
+        $empData =  [
+            'first_name' =>$request->fname,
+            'last_name' =>$request->lname,
+            'email' =>$request->email,
+            'phone' =>$request->phone,
+            'post' =>$request->post,
+            'avatar' =>$fileName,
+        ];
+
+
+        Employee::create($empData);
+        return response()->json([
+            'status' => 200,
+
+        ]);
+
+
+
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function FetchAll()
+
+    {
+        $emps = Employee::all();
+		$output = '';
+		if ($emps->count() > 0) {
+			$output .= '<table class="table table-striped table-sm text-center align-middle">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Avatar</th>
+                <th>Name</th>
+                <th>E-mail</th>
+                <th>Post</th>
+                <th>Phone</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>';
+			foreach ($emps as $emp) {
+				$output .= '<tr>
+                <td>' . $emp->id . '</td>
+                <td><img src=/image/'.$emp->avatar . '" width="50" class="img-thumbnail rounded-circle"></td>
+                <td>' . $emp->first_name . ' ' . $emp->last_name . '</td>
+                <td>' . $emp->email . '</td>
+                <td>' . $emp->post . '</td>
+                <td>' . $emp->phone . '</td>
+                <td>
+                  <a href="#" id="' . $emp->id . '" class="text-success mx-1 editIcon" data-bs-toggle="modal" data-bs-target="#editEmployeeModal"><i class="bi-pencil-square h4"></i></a>
+
+                  <a href="#" id="' . $emp->id . '" class="text-danger mx-1 deleteIcon"><i class="bi-trash h4"></i></a>
+                </td>
+              </tr>';
+			}
+			$output .= '</tbody></table>';
+			echo $output;
+		} else {
+			echo '<h1 class="text-center text-secondary my-5">No record present in the database!</h1>';
+		}
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Employee $employee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Employee  $employee
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Employee $employee)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Crud Ajax</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.11.5/datatables.min.css"/>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+</head>
+<body>
+
+    <div class="modal fade" id="addEmployeeModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    data-bs-backdrop="static" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Add New Employee</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form action="#" method="POST" id="add_employee_form" enctype="multipart/form-data">
+          @csrf
+          <div class="modal-body p-4 bg-light">
+            <div class="row">
+              <div class="col-lg">
+                <label for="fname">First Name</label>
+                <input type="text" name="fname" class="form-control" placeholder="First Name" required>
+              </div>
+              <div class="col-lg">
+                <label for="lname">Last Name</label>
+                <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
+              </div>
+            </div>
+            <div class="my-2">
+              <label for="email">E-mail</label>
+              <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+            </div>
+            <div class="my-2">
+              <label for="phone">Phone</label>
+              <input type="tel" name="phone" class="form-control" placeholder="Phone" required>
+            </div>
+            <div class="my-2">
+              <label for="post">Post</label>
+              <input type="text" name="post" class="form-control" placeholder="Post" required>
+            </div>
+            <div class="my-2">
+              <label for="avatar">Select Avatar</label>
+              <input type="file" name="avatar" class="form-control" required>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" id="add_employee_btn" class="btn btn-primary">Add Employee</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  {{-- add new employee modal end --}}
+
+  {{-- edit employee modal start --}}
+  <div class="modal fade" id="editEmployeeModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    data-bs-backdrop="static" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit Employee</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form action="#" method="POST" id="edit_employee_form" enctype="multipart/form-data">
+          @csrf
+          <input type="hidden" name="emp_id" id="emp_id">
+          <input type="hidden" name="emp_avatar" id="emp_avatar">
+          <div class="modal-body p-4 bg-light">
+            <div class="row">
+              <div class="col-lg">
+                <label for="fname">First Name</label>
+                <input type="text" name="fname" id="fname" class="form-control" placeholder="First Name" required>
+              </div>
+              <div class="col-lg">
+                <label for="lname">Last Name</label>
+                <input type="text" name="lname" id="lname" class="form-control" placeholder="Last Name" required>
+              </div>
+            </div>
+            <div class="my-2">
+              <label for="email">E-mail</label>
+              <input type="email" name="email" id="email" class="form-control" placeholder="E-mail" required>
+            </div>
+            <div class="my-2">
+              <label for="phone">Phone</label>
+              <input type="tel" name="phone" id="phone" class="form-control" placeholder="Phone" required>
+            </div>
+            <div class="my-2">
+              <label for="post">Post</label>
+              <input type="text" name="post" id="post" class="form-control" placeholder="Post" required>
+            </div>
+            <div class="my-2">
+              <label for="avatar">Select Avatar</label>
+              <input type="file" name="avatar" class="form-control">
+            </div>
+            <div class="mt-2" id="avatar">
+
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" id="edit_employee_btn" class="btn btn-success">Update Employee</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  {{-- edit employee modal end --}}
+
+  <body class="bg-light">
+    <div class="container">
+      <div class="row my-5">
+        <div class="col-lg-12">
+          <div class="card shadow">
+            <div class="card-header bg-danger d-flex justify-content-between align-items-center">
+              <h3 class="text-light">Manage Employees</h3>
+              <button class="btn btn-light" data-bs-toggle="modal" data-bs-target="#addEmployeeModal"><i
+                  class="bi-plus-circle me-2"></i>Add New Employee</button>
+            </div>
+            <div class="card-body" id="show_all_employees">
+              <h1 class="text-center text-secondary my-5">Loading...</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.5/datatables.min.js"></script>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
+
+    <script>
+
+//fetchh all employee
+FetchEmployee();
+ function FetchEmployee(){
+
+    $.ajax({
+
+        url: '{{ route('fetchAll') }}',
+          method: 'get',
+          success: function(response) {
+
+            $("#show_all_employees").html(response);
+            $("table").DataTable({
+              order: [0, 'desc']
+            });
+          }
+
+    });
+
+ }
+
+
+// make new employee
+
+$("#add_employee_form").submit(function(e){
+e.preventDefault();
+
+const fd = new FormData(this)
+$("#add_employee_btn").text("Adding...")
+$.ajax({
+  url: '{{ route('employee.store') }}',
+  method: 'post',
+  data: fd,
+  cache: false,
+  processData: false,
+  contentType: false,
+  success:function(res){
+      if(res.status == 200){
+
+        swal.fire(
+            'Added',
+            'Employee Added Succesfully!',
+            'success'
+
+        )
+
+      }
+      $('#add_employee_btn').text('Add Employee');
+      $('#add_employee_form')[0].reset();
+      $('#addEmployeeModal').modal('hide');
+      FetchEmployee();
+      console.log(res);
+  }
+});
+});
+
+    </script>
+</body>
+</html>
